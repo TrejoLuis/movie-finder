0b3e4a18c817835969942ff1784fe85e802a2bdc
Luis Trejo <trejorias@gmail.com>
2023-07-14 12:22:27 -0600

    feat: add input validation, first time input do not show blank input error, encapsulate all logic related to movies to the custom hook useMovies

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,18 @@ import Movies from './components/Movies.jsx'
 import { useMovies } from './hooks/useMovies'
 
 function App () {
-  const { movies } = useMovies()
+  const { movies, error, search, updateSearch, submitChange } = useMovies()
 
   return (
     <div className='app'>
       <header>
         <h1>Movie Finder</h1>
-        <form>
-          <input placeholder='Shrek, The Matrix, Harry Potter...' />
+        <form onSubmit={submitChange}>
+          <input onChange={updateSearch} value={search} placeholder='Shrek, The Matrix, Harry Potter...' />
           <button type='submit'>Search</button>
         </form>
+        {error &&
+          <p style={{ color: 'salmon' }}>{error}</p>}
       </header>
       <main>
         <Movies movies={movies}/>
--- src/hooks/useMovies.js
+++ src/hooks/useMovies.js
@@ -1,7 +1,12 @@
 import goodResponseResults from '../mocks/goodResult.json'
+import { useState, useEffect, useRef } from 'react'
 
 // Custom hook to manage fetching, data mapping, states
 export function useMovies () {
+  const [search, setSearch] = useState('')
+  const [error, setError] = useState()
+  const isFirstInput = useRef(true)
+
   const rawMovies = goodResponseResults?.Search
   const mappedMovies = rawMovies.map(movie => ({
     id: movie.imdbID,
@@ -10,5 +15,43 @@ export function useMovies () {
     poster: movie.Poster
   }))
 
-  return { movies: mappedMovies }
+  function handleSubmit (e) {
+    e.preventDefault()
+    console.log(search)
+    //FETCHING
+  }
+
+  function handleChange (e) {
+    const newSearch = e.target.value
+    if(newSearch === ' ') return
+    setSearch(newSearch)
+
+  }
+
+  useEffect(() => {
+    //validating first time input
+    if(isFirstInput.current){
+      isFirstInput.current = search === ''
+      return
+    }
+    // validating input
+    if (search === '') {
+      setError('Please write a movie to find')
+      return
+    }
+
+    if(search.length < 3){
+      setError('Please write at least 2 characters')
+      return
+    }
+
+    setError(null)
+  },[search])
+
+
+  return { movies: mappedMovies, 
+    error, 
+    search, 
+    updateSearch: handleChange,
+    submitChange: handleSubmit}
 }
