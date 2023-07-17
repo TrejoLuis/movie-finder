50351828a0a1362d29f450d3327b05dbe529a052
Luis Trejo <trejorias@gmail.com>
2023-07-17 14:34:51 -0600

    feat: returns mapped movies based on a search

--- src/hooks/useMovies.js
+++ src/hooks/useMovies.js
@@ -1,57 +1,18 @@
-import goodResponseResults from '../mocks/goodResult.json'
-import { useState, useEffect, useRef } from 'react'
+import { searchMovies } from '../services/movies'
+import { useState } from 'react'
 
 // Custom hook to manage fetching, data mapping, states
-export function useMovies () {
-  const [search, setSearch] = useState('')
-  const [error, setError] = useState()
-  const isFirstInput = useRef(true)
+export function useMovies ({ search }) {
+  const [moviesRes, setMoviesRes] = useState([])
 
-  const rawMovies = goodResponseResults?.Search
-  const mappedMovies = rawMovies.map(movie => ({
-    id: movie.imdbID,
-    title: movie.Title,
-    year: movie.Year,
-    poster: movie.Poster
-  }))
-
-  function handleSubmit (e) {
-    e.preventDefault()
+  function getMovies () {
     console.log(search)
-    //FETCHING
+    searchMovies({search})
+      .then(movies => setMoviesRes(movies))
   }
 
-  function handleChange (e) {
-    const newSearch = e.target.value
-    if(newSearch === ' ') return
-    setSearch(newSearch)
-
+  return {
+    movies: moviesRes,
+    getMovies
   }
-
-  useEffect(() => {
-    //validating first time input
-    if(isFirstInput.current){
-      isFirstInput.current = search === ''
-      return
-    }
-    // validating input
-    if (search === '') {
-      setError('Please write a movie to find')
-      return
-    }
-
-    if(search.length < 3){
-      setError('Please write at least 2 characters')
-      return
-    }
-
-    setError(null)
-  },[search])
-
-
-  return { movies: mappedMovies, 
-    error, 
-    search, 
-    updateSearch: handleChange,
-    submitChange: handleSubmit}
 }
