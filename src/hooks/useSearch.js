1b49c29f9ce5b3083ca3d748920f0b04bdb93567
Luis Trejo <trejorias@gmail.com>
2023-07-17 14:33:46 -0600

    feat: add useSearch hook to handle user input and handle visual error

---
+++ src/hooks/useSearch.js
@@ -0,0 +1,39 @@
+import { useState, useEffect, useRef } from 'react'
+
+export function useSearch () {
+  const [search, setSearch] = useState('')
+  const [error, setError] = useState()
+  const isFirstInput = useRef(true)
+
+  useEffect(() => {
+    // validating first time input
+    if (isFirstInput.current) {
+      isFirstInput.current = search === ''
+      return
+    }
+    // validating input
+    if (search === '') {
+      setError('Please write a movie to find')
+      return
+    }
+
+    if (search.length < 3) {
+      setError('Please write at least 2 characters')
+      return
+    }
+
+    setError(null)
+  }, [search])
+
+  function updateSearch (newSearch) {
+    if (newSearch === ' ') return
+    setSearch(newSearch)
+    console.log(newSearch)
+  }
+
+  return {
+    search,
+    error,
+    updateSearch
+  }
+}
